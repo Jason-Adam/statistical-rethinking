904bf4b5503f0a35836f65ede23682b37a2fe004
Jason Adam <22adamj22@gmail.com>
2020-06-24 23:29:01 -0700

    added boilerplate for quadratic approximation

--- src/approximation/grid_approximation.R
+++ src/approximation/grid_approximation.R
@@ -2,9 +2,11 @@
 num_points <- 1000
 
 # Define grid
-p_grid <- seq(from = 0,
-              to = 1,
-              length.out = num_points)
+p_grid <- seq(
+  from = 0,
+  to = 1,
+  length.out = num_points
+)
 
 # Define prior
 prior <- rep(1, num_points)
@@ -20,11 +22,12 @@ posterior <- unstandard_posterior / sum(unstandard_posterior)
 
 # Plot
 png("imgs/grid_approximation_01.png")
-plot(p_grid ,
-     posterior ,
-     type = "b" ,
-     xlab = "probability of water" ,
-     ylab = "posterior probability",
-     main = "Posterior Probability")
+plot(p_grid,
+  posterior,
+  type = "b",
+  xlab = "probability of water",
+  ylab = "posterior probability",
+  main = "Posterior Probability"
+)
 mtext(paste(num_points, " points"))
 dev.off()
---
+++ src/approximation/quadratic_approximation.R
@@ -0,0 +1,5 @@
+library(rethinking)
+
+globe_qa <- quap(alist(W ~ dbinom(W + L , p),
+                       p ~ dunif(0, 1)),
+                 data = list(W = 6, L = 3))
